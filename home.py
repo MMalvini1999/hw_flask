247bbfb412ebf7a691c18dafec114f1dc11eddd7
Matthew Malvini <matthew.malvini@sjsu.edu>
2022-03-25 16:58:37 -0700

    home.py has been finalized

--- home.py
+++ home.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import escape
 
 myapp_obj = Flask(__name__)
 
-@myapp_obj.route("/home")
+@myapp_obj.route("/")
 def home():
-	return "Home Page"
+	name="Lisa"
+	city_names=['Paris', 'London', 'Rome', 'Tahiti']
+	return f"""
+        <h1>Welcome {name}!</h1>
+        <a href="https://www.google.com">not google</a>
+
+        <ul>
+                <li>{city_names[0]}</li>
+                <li>{city_names[1]}</li>
+                <li>{city_names[2]}</li>
+                <li>{city_names[3]}</li>
+        </ul>"""
 myapp_obj.run()
 
