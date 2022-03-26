e86d3656a3a849c06bf1e01b584f1f16defe95bb
Matthew Malvini <matthew.malvini@sjsu.edu>
2022-03-25 17:10:35 -0700

    home.py now has been finalized for LP question

--- home.py
+++ home.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 from flask import escape
 
 myapp_obj = Flask(__name__)
+name="Lisa"
+city_names=['Paris', 'London', 'Rome', 'Tahiti']
 
 @myapp_obj.route("/")
 def home():
-	name="Lisa"
-	city_names=['Paris', 'London', 'Rome', 'Tahiti']
 	return f"""
         <h1>Welcome {name}!</h1>
         <a href="https://www.google.com">not google</a>
